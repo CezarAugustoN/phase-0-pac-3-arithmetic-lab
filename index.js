5a63be5ceeb33e45c502d190ac3893923530a709
CezarAugustoNeves <creatingafuture94@gmail.com>
2023-03-18 23:54:18 -0400

    adds

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide(){}
+function add(a,b){
+    return a+b
+}
+function subtract(a,b){
+    return a-b
+}
+function multiply(a,b){
+    return a*b
+}
+function divide(a,b){
+    return a/b
+}
+function increment(_n){}
+function increment(n){
+    return(++n)
+}
+function decrement(_n){}
+function decrement(n){
+    return(--n)
+}
+
+
+
+function makeInt(n){
+    return parseInt(n, 10)
+    }
+
+
+function preserveDecimal(n){
+    return parseFloat(n, 2.222)
+}
+
+
+
+
+
+
